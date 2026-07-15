3b91b835861143f5e6031772e213b4d77037beca
batakpout <16462830+batakpout@users.noreply.github.com>
2026-07-16 00:31:50 +0530

    Add files via upload

---
+++ recursion/concepts/number_spell.py
@@ -0,0 +1,16 @@
+def print_spell(num):
+    spell = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+
+    def loop(n):
+        if n <= 0:
+            return
+        last_digit = n % 10
+        loop(n // 10)
+        print(spell[last_digit])
+
+    loop(num)
+
+
+if __name__ == "__main__":
+    num1 = 2021
+    print_spell(num1)
