31b4c107c603b4c5ced0b301b1b85551f5eaac48
batakpout <16462830+batakpout@users.noreply.github.com>
2026-07-20 22:48:59 +0530

    Update ReverseArrayRecursive.py

--- recursion/concepts/ReverseArrayRecursive.py
+++ recursion/concepts/ReverseArrayRecursive.py
@@ -1,45 +1,31 @@
 """
-reverse a stack
+* Time: O(n^2)
+* Space: O(n) (because of recursion)
 """
-
-def insert_at_bottom(stack: list[int], data_to_insert: int):
-    if not stack:
-        stack.append(data_to_insert)
-        return None
-    top = stack[-1]
-    stack.pop()
-    insert_at_bottom(stack, data_to_insert)
-    stack.append(top)
-    return stack
-
-
-def sort_stack(stack: list[int]):
-    if not stack:
+def sort_insert(arr: list[int], temp):
+    if not arr or arr[-1] <= temp:
+        arr.append(temp)
         return
-    top = stack[-1]
-    stack.pop()
-    sort_stack(stack)
-    insert_at_bottom(stack, top)
 
-def display_stack(stack: list[int]):
+    last = arr.pop()
+    sort_insert(arr, temp)
+    arr.append(last)
 
-    print("Top")
-    print("  ---")
-    for item in reversed(stack):
-        print(f"| {item} |")
-    print("  ---")
-    print("Bottom")
+
+def recursive_sort(arr: list[int]):
+    if len(arr) <= 1:
+        return
+
+    temp = arr.pop()
+    recursive_sort(arr)
+    sort_insert(arr, temp)
 
 def main():
 
-    stack = [40, 30, 20, 10]  # 10 is the top of the stack
-    print("Original Stack:")
-    display_stack(stack)
-    print(f"\nsorted stack\n")
-
-    sort_stack(stack)
-
-    display_stack(stack)
+    arr = [5, 2, 41, 3]
+    print("Before:", arr)
+    recursive_sort(arr)
+    print("After :", arr)
 
 if __name__ == "__main__":
     main()
