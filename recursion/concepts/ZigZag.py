94d048e60b4f82a81a62b6d29ef3c995aaf9758a
batakpout <16462830+batakpout@users.noreply.github.com>
2026-07-22 01:08:21 +0530

    Add files via upload

---
+++ recursion/concepts/ZigZag.py
@@ -0,0 +1,31 @@
+count = 0
+
+"""
+TC: =2^(n+1) - 1 we can use recurrence proof for same
+
+Let T(n) be the number of function calls.
+For every call with n > 0:
+* current call contributes 1
+* first recursive call contributes T(n-1)
+* second recursive call contributes T(n-1)
+
+So
+T(n)=2T(n-1)+1
+T(0)=1 equals,
+2^(n+1) - 1
+"""
+def zig_zag(n):
+    global count
+    count += 1
+    if n == 0:
+        return
+    print(f"Pre: {n}")
+    zig_zag(n - 1)
+    print(f"In: {n}")
+    zig_zag(n - 1)
+    print(f"Post: {n}")
+
+
+if __name__ == "__main__":
+    zig_zag(4)
+    print(count)
