0f011d4e4385b56bf39d4ba1742e06f2e7e76313
batakpout <16462830+batakpout@users.noreply.github.com>
2026-08-08 00:34:03 +0530

    Update SubSets.py

--- recursion/concepts/SubSets.py
+++ recursion/concepts/SubSets.py
@@ -2,13 +2,25 @@
 
 """
 Used input output method i.e make input small and make a recursive tree
+
+"""
+r"""
+                         ("", "ABC")
+                        /            \
+              ("", "BC")              ("A", "BC")
+              /        \              /           \
+        ("","C")    ("B","C")    ("A","C")    ("AB","C")
+         /    \      /    \       /    \        /     \
+       ("","") ("C","") ("B","") ("BC","") ("A","") ("AC","") ("AB","") ("ABC","")
+          |       |        |         |        |        |          |          |
+         ""       C        B        BC        A        AC        AB        ABC
 """
 
 """
 General rule: abc
 subsets : '', a, b, c, ab, ac, bc, abc. Here ac can be written as ca but this is not allowed in subsequence
 substring: contagious i.e a, b, c, ab, bc, abc , but not ac
-subsequence: a, b, c, ab, bc, abc, ac,  but not ca
+subsequence: a, b, c, ab, bc, abc, ac  but not ca
 
 So if we asked to print subsequences in question, subset only we can print, the way we did below
 but, printing sub-strings could be a different question, how to do it?
@@ -30,31 +42,16 @@ space: O(N) recursive call stack
 """
 Recursive technique:
 choice + decision
-
-make input smaller each step, input-output method
 """
 
-def all_subsets(output, input):
-    if len(input) == 0:
-        print(output, end=" ")
-        return
-    op1 = output
-    op2 = output  # direct op2 += input[0] not allowed by python, have to create it here first
 
-    op2 += input[0]
-    input = input[:1]
-    all_subsets(op1, input)
-    all_subsets(op2, input)
-    return  # not needed
-
-
-def concise_version_all_subsets(output, input_str):
+def all_subsets(output, input_str):
     if len(input_str) == 0:
         print(output)
         return
 
-    concise_version_all_subsets(output, input_str[1:])
-    concise_version_all_subsets(output + input_str[0], input_str[1:])
+    all_subsets(output, input_str[1:])
+    all_subsets(output + input_str[0], input_str[1:])
     return
 
 
@@ -65,7 +62,7 @@ unique output: {'', 'a', 'b', 'aab', 'aa', 'ab'}
 """
 
 
-def unique_concise_version_all_subsets(input_str) -> set[int]:
+def all_subsets_accumulate(input_str) -> set[int]:
     seen = set()
 
     def helper(output, input_str):
@@ -81,9 +78,9 @@ def unique_concise_version_all_subsets(input_str) -> set[int]:
 
 
 if __name__ == "__main__":
-    concise_version_all_subsets("", "aab")
+    all_subsets("", "aab")
     print("*" * 30)
     # concise_version("", "abc")
     print("*" * 30)
     # concise_version("", "abcd")
-    print(unique_concise_version_all_subsets("aab"))
+    print(all_subsets_accumulate("aab"))
