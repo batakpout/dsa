bed6486d75e4118a3a8bb6c79e6ee556c78bb3f3
batakpout <16462830+batakpout@users.noreply.github.com>
2026-08-08 00:07:11 +0530

    Update PermutationsOfString.py

--- backtracking/PermutationsOfString.py
+++ backtracking/PermutationsOfString.py
@@ -1,3 +1,27 @@
+"""
+Time-Complexity:-
+
+How many recursive calls?
+At each level:
+Level 0: (n) choices e.g n = 4
+Level 1: (n-1) choices , total:- n*(n-1), 4 * 3 = 12
+Level 2: (n-2) choices, total :- n*(n-1)(n-2), 4*3*2 = 24
+...
+at last: 1 choice , 4*3*2*1 = 24
+So the number of leaves is:-
+4+12+24+24 = 24 we took dominating term, so 24 i.e n!
+
+Time complexity = work done per node * total nodes
+
+work done per node = string ops so O(N) [The for loop is already represented within the recursion tree, so we don't need
+ to factor it separately into the complexity]
+
+ Therefore, the overall complexity is O(n × n!), not O(n² × n!)]
+
+ Space complexity: O(n * n!) [result list] , n! is total strings with each of size n, this is dominating factor
+ we ignore small ones e.g O(N) recursive stack, temporary strings formed 
+"""
+
 def using_recursion(s: str) -> list[str]:
     result = []
 
